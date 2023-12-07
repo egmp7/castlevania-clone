73e2de1029a4804f337a4358a94710fc243a2257
egmp7 <deweyerick@gmail.com>
2023-12-07 16:34:40 -0600

    restart scene

---
+++ Assets/Scripts/RestartScene.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScene : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        EventManager.onPlayerDeath += Restart;
+    }
+    private void OnDisable()
+    {
+        EventManager.onPlayerDeath -= Restart;
+    }
+
+    private void Restart()
+    {
+        Invoke("LoadScene", 2f);
+        
+    }
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
